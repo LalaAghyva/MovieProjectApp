315a9922b1293fb774feaabc99abcc0c098c4986
LalaAghyva <laleagayeva78@gmail.com>
2024-07-25 20:25:41 +0400

    Initial Commit

---
+++ MovieProjectApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieProjectApp
+//
+//  Created by Lala on 25.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
